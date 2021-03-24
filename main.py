29192fc830125e7ce9dc85d8053c837c72d11a61
MarZwa <mgm.zwaan@gmail.com>
2021-03-24 08:41:15 +0100

    mysql.connector terug gezet

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
-# import mysql.connector
+import mysql.connector
 import time
 
 import serial
 import os
 
-# mydb = mysql.connector.connect(
-#     host="localhost",
-#     user="marc",
-#     passwd="password",
-#     database="SmartKitchenDb"
-# )
+mydb = mysql.connector.connect(
+    host="localhost",
+    user="marc",
+    passwd="password",
+    database="SmartKitchenDb"
+)
 port = serial.Serial("/dev/ttyUSB0", baudrate=9600, timeout=3.0)
 
-# mycursor = mydb.cursor
+mycursor = mydb.cursor
 
 while True:
     weight = port.readline().split()
@@ -20,6 +20,6 @@ while True:
         mycursor.execute("UPDATE user_foods WHERE name == " + weight[0] + " SET " + weight[2] + " = " + weight[2] + " + " + weight[3])
 
     time.sleep(1)
-    # mydb.commit()
+    mydb.commit()
 
-# mydb.close()
+mydb.close()
