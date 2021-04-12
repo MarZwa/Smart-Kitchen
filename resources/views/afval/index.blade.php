eb4725c53b5c1e0bdb6e88581627300279261998
Jim-Zweers <JimboZweers@gmail.com>
2021-04-12 13:16:39 +0100

    responsive voor tablet

--- resources/views/afval/index.blade.php
+++ resources/views/afval/index.blade.php
@@ -28,56 +28,63 @@
 
         <article class="bakken">
 
-        @if($status->status == 'Rest')
-            <section class="bakken_card u-back-grey">
-        @else
-            <section class="bakken_card">
-        @endif
-                <figure class="bakken_card__fig">
-                    <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
-                    <figcaption>Rest</figcaption>  
-                </figure>
-            </section>
+        <section>
+            @if($status->status == 'Rest')
+                <article class="bakken_card u-back-grey">
+            @else
+                <article class="bakken_card">
+            @endif
+                    <figure class="bakken_card__fig">
+                        <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
+                        <figcaption>Rest</figcaption>  
+                    </figure>
+                </article>
 
-        @if($vol_rest-> vol == 1)
-            <section class="bakken_volCard u-back-grey">
-                <p>De rest bak is vol {{ $user_rest->name }} moet hem legen</p>
-            </section>
-        @endif
+            @if($vol_rest-> vol == 1)
+                <article class="bakken_volCard u-back-grey">
+                    <p>De rest bak is vol {{ $user_rest->name }} moet hem legen</p>
+                </article>
+            @endif
+        </section>
 
-        @if($status->status == 'Plastic')
-            <section class="bakken_card u-back-orange">
-        @else
-            <section class="bakken_card">
-        @endif
-                <figure class="bakken_card__fig">
-                    <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
-                    <figcaption>Plastic</figcaption>  
-                </figure>
-            </section>
-        
-        @if($vol_plastic-> vol == 1)
-            <section class="bakken_volCard u-back-orange">
-                <p>De plastic bak is vol {{ $user_plastic->name }} moet hem legen</p>
-            </section>
-        @endif
+        <section>
+            @if($status->status == 'Plastic')
+                <article class="bakken_card u-back-orange">
+            @else
+                <article class="bakken_card">
+            @endif
+                    <figure class="bakken_card__fig">
+                        <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
+                        <figcaption>Plastic</figcaption>  
+                    </figure>
+                </article>
+            
+            @if($vol_plastic-> vol == 1)
+                <article class="bakken_volCard u-back-orange">
+                    <p>De plastic bak is vol {{ $user_plastic->name }} moet hem legen</p>
+                </article>
+            @endif
+        </section>
 
-        @if($status->status == 'Gft')
-            <section class="bakken_card u-back-green">
-        @else
-            <section class="bakken_card">
-        @endif
-                <figure class="bakken_card__fig">
-                    <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
-                    <figcaption>Gft</figcaption>  
-                </figure>
-            </section>
-        
-        @if($vol_gft-> vol == 1)
-            <section class="bakken_volCard u-back-green">
-                <p>De gft bak is vol {{ $user_gft->name }} moet hem legen</p>
-            </section>
-        @endif
+        <section>
+            @if($status->status == 'Gft')
+                <article class="bakken_card u-back-green">
+            @else
+                <article class="bakken_card">
+            @endif
+                    <figure class="bakken_card__fig">
+                        <img class="bakken_card__img" src="/img/Vuilnisbak.webp" alt="Foto van een vuilnisbak">
+                        <figcaption>Gft</figcaption>  
+                    </figure>
+                </article>
+            
+            @if($vol_gft-> vol == 1)
+                <article class="bakken_volCard u-back-green">
+                    <p>De gft bak is vol {{ $user_gft->name }} moet hem legen</p>
+                </article>
+            @endif
+        </section>
+
         </article>
     </body>
 </html>
