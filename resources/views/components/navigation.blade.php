09ddc938251e6ecbb4ae119933c0ce42389c09b1
Baeshee <basbarg@gmail.com>
2021-04-15 01:13:44 +0200

    Smart Kitchen link now in absolute position so its always available

--- resources/views/components/navigation.blade.php
+++ resources/views/components/navigation.blade.php
@@ -1,11 +1,11 @@
 <nav class="navigation u-z-2">
+    <a class="home-logo u-z-3" href="/">Smart Kitchen</a>
     <div class="navigation-icon u-z-3" onclick="changeIcon()">
         <div class="top"></div>
         <div class="middle"></div>
         <div class="bottom"></div>
     </div>
     <ul class="navigation__menu u-z-2">
-        <li class="navigation__menu__item"><a class="dashboard-navigation__menu__link" href="/">Smart Kitchen</i></a></li>
         <li class="navigation__menu__item"><a class="dashboard-navigation__menu__link" href="/">Afval</a></li>
         <li class="navigation__menu__item"><a class="dashboard-navigation__menu__link" href="/">Tafel Dekken</a></li>
         <li class="navigation__menu__item"><a class="dashboard-navigation__menu__link" href="/users">Profiles</a></li>
