24d256d6e73afeddc6e3e5caa9469fc020ab1999
Baeshee <basbarg@gmail.com>
2021-04-14 14:40:43 +0200

    Edit to the display of the daily scanned products

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -18,7 +18,7 @@ class UsersController extends Controller
     public function show($id){
         return view('users.show', [
             'user' => User::find($id),
-            'products' => User::find($id)->allProducts->where('date', Carbon::now()->format('d-m-Y'))->take(6),
+            'products' => User::find($id)->allProducts->where('date', Carbon::now()->format('d-m-Y')),
         ]);
     }
 
