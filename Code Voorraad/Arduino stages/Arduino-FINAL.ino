f51e81384ac01bc6e0dd3ad49d87e40789ad873b
MaxVssr <max.visser576@gmail.com>
2021-04-16 16:02:11 +0100

    Added final comments

--- Code Voorraad/Arduino stages/Arduino-FINAL.ino
+++ Code Voorraad/Arduino stages/Arduino-FINAL.ino
@@ -2,14 +2,13 @@
 #include "MFRC522.h" // RFID library (https://github.com/miguelbalboa/rfid)
 #include "Nextion.h"
 
+//pins voor de RFID reader definiëren
 const int pinRST = 9;
 const int pinSDA = 10;
 
-const int BUTTON = 4;
-int oldButtonState = LOW;
-
 MFRC522 mfrc522(pinSDA, pinRST); // Set up mfrc522 on the Arduino
 
+//verwijzing naar de buttons op mijn touch display
 int currentPage = 1;
 NexButton b0 = NexButton(1,1,"b0");
 NexButton b1 = NexButton(1,2,"b1");
@@ -21,6 +20,7 @@ NexTouch *nex_listen_list[] = {
 NULL
 };
 
+//functie die er voor zorgt dat de buttons op pagina 1 een serial print sturen naar de arduino
 void b0PushCallback(void *ptr) {
   Serial.print("page 1");
   Serial.print(0xff);
@@ -38,6 +38,7 @@ void b0PushCallback(void *ptr) {
   Serial.println("S");
 }
 
+//functie die er voor zorgt dat de buttons op pagina 1 een serial print sturen naar de arduino
 void b1PushCallback(void *ptr) {
   Serial.print("page 1");
   Serial.print(0xff);
@@ -56,7 +57,6 @@ void b1PushCallback(void *ptr) {
 }
 
 void setup() {
-  pinMode(BUTTON, INPUT_PULLUP);
   SPI.begin(); // open SPI connection
   mfrc522.PCD_Init();
   Serial.begin(9600);
@@ -65,6 +65,7 @@ void setup() {
   b1.attachPush(b1PushCallback);
 }
 
+//code voor het printen van de RFID
 void loop() {
   nexLoop(nex_listen_list);
 
